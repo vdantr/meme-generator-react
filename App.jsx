f6e4b54e16eb9e2e01d6a8e75ab77211882abcc8
vdantr <veselin.trifonov.tt@gmail.com>
2023-02-15 14:09:31 +0100

    add Header component

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "./components/Header.jsx"
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
